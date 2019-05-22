5dcda623e87b6792f99b3a3ab126f7fbaf7d6020
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-05-22 11:32:57 +0530

    Structure and Comment Added

--- nlp_dec.py
+++ nlp_dec.py
@@ -4,17 +4,17 @@ Created on Tue May 21 11:21:00 2019
 
 @author: tanma
 """
-
+# Lib Imports
 import re
-import pandas as pd, matplotlib.pyplot as plt,numpy as np
+import pandas as pd, matplotlib.pyplot as plt
 from keras.initializers import VarianceScaling
 from dec import DEC
 from collections import Counter
 from wordcloud import WordCloud
 from keras.optimizers import SGD
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
-
+# Data Read and Sort
 data = pd.read_csv("Feedback 2019.csv", header = None)
 
 data.iloc[:,3] = pd.to_datetime(data.iloc[:,3])
@@ -28,6 +28,26 @@ data = data[:100000]
 
 stopwords = ['q','p','ans','remark','comment',' ans',' q',' remark',' comment',' p','do you want to share any additional feedback']
 
+# Wordcloud make and Plot Func
+def make_wordcloud(optimum,arr,a,str_lit):
+    for j in range(optimum):
+        n = []
+        for i in arr['Conversation'][arr[a] == j]:
+            n.append(i)
+        
+        wordcloud = WordCloud(width = 800, height = 800, 
+                        background_color ='white', 
+                        stopwords = stopwords, 
+                        min_font_size = 10).generate(' '.join(n)) 
+        
+        plt.figure(figsize = (8, 8), facecolor = None) 
+        plt.imshow(wordcloud) 
+        plt.axis("off") 
+        plt.tight_layout(pad = 0) 
+        plt.savefig(str_lit + str(j), dpi = 500)  
+        plt.show()
+
+# Stopwords Removed
 med = []
 for i in data['Conversation']:
     review = re.sub('[^a-zA-Z]', ' ', str(i))
@@ -41,6 +61,7 @@ copy = data
 
 copy['Conversation'] = med 
 
+# Cluster Level 1 and 2 added to DataFrame
 a = []
 b = []
 for i in copy['Conversation']:
@@ -68,47 +89,54 @@ for i in copy['Satisfaction Feedback']:
 copy['Cluster Level 2'] = c
 copy = copy[copy['Satisfaction Feedback'] != ' ']
 
-copy_actual = copy[copy['Cluster Level 2'] == 1]
+# Data made for Model
+def preprocess(literal,Level,arr):
+    copy_actual = arr[arr[literal] == Level]
 
-actual = []
-for i in copy_actual['Conversation']:
-    review = i[4:]
-    review = ' '.join(review)
-    actual.append(review)
-
-copy_actual['Conversation'] = actual
-
-actual_words = []
-for i in actual:
-    review = i.split()
-    for j in review:
-        actual_words.append(j)
-
-counts = Counter(actual_words)
-
-actual_stopwords = []
-for i,j in zip(counts,counts.values()):
-    if(j < 2 or j > 1000):
-        actual_stopwords.append(i)
-        
-actual_split = []
-for i in copy_actual['Conversation']:
-    review = i.split()
-    review = [word for word in review if not word in actual_stopwords]
-    actual_split.append(' '.join(review))
-
-copy_actual['Conversation'] = actual_split
-copy_actual = copy_actual[copy_actual['Conversation'] != '']
-
-while '' in actual_split:
-    actual_split.remove('')
+    actual = []
+    for i in copy_actual['Conversation']:
+        review = i[4:]
+        review = ' '.join(review)
+        actual.append(review)
     
+    copy_actual['Conversation'] = actual
+    
+    actual_words = []
+    for i in actual:
+        review = i.split()
+        for j in review:
+            actual_words.append(j)
+    
+    counts = Counter(actual_words)
+    
+    actual_stopwords = []
+    for i,j in zip(counts,counts.values()):
+        if(j < 2 or j > 1000):
+            actual_stopwords.append(i)
+            
+    actual_split = []
+    for i in copy_actual['Conversation']:
+        review = i.split()
+        review = [word for word in review if not word in actual_stopwords]
+        actual_split.append(' '.join(review))
+    
+    copy_actual['Conversation'] = actual_split
+    copy_actual = copy_actual[copy_actual['Conversation'] != '']
+    
+    while '' in actual_split:
+        actual_split.remove('')
+    
+    cv = CountVectorizer()
+    X = cv.fit_transform(actual_split).toarray()
+    V = cv.vocabulary_
+    B = cv.get_feature_names()
+        
+    return X,V,B,copy_actual
 
-cv = CountVectorizer()
-X = cv.fit_transform(actual_split).toarray()
-V = cv.vocabulary_
-B = cv.get_feature_names()
 
+X,_,_,copy_actual = preprocess('Cluster Level 2',1,copy)
+
+# Hyperparameters Defined
 init = 'glorot_uniform'
 pretrain_optimizer = 'adam'
 dataset = 'mnist'
@@ -124,7 +152,8 @@ if not os.path.exists(save_dir):
 update_interval = 50
 pretrain_epochs = 100
 init = VarianceScaling(scale=1. / 3., mode='fan_in',
-                       distribution='uniform')  # [-limit, limit], limit=sqrt(1./fan_in)
+                       distribution='uniform')  
+# [-limit, limit], limit=sqrt(1./fan_in)
 #pretrain_optimizer = SGD(lr=1, momentum=0.9)
 
 dec = DEC(dims=[X.shape[-1], 500, 500, 2000, 10], n_clusters=3, init=init)
@@ -142,61 +171,9 @@ y_pred = dec.fit(X, tol=tol, maxiter=maxiter, batch_size=batch_size,
 
 copy_actual['Cluster Level 3'] = list(y_pred)
 
-for j in range(3):
-    n = []
-    for i in copy_actual['Conversation'][copy_actual['Cluster Level 3'] == j]:
-        n.append(i)
-    
-    wordcloud = WordCloud(width = 800, height = 800, 
-                    background_color ='white', 
-                    stopwords = stopwords, 
-                    min_font_size = 10).generate(' '.join(n)) 
-    
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud) 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
-    plt.savefig('Satisfied DEC WordCloud for Cluster '+str(j), dpi = 500)  
-    plt.show()
+make_wordcloud(3,copy_actual,'CLuster Level 3','Satisfied DEC Cluster')
 
-copy_actual_not = copy[copy['Cluster Level 2'] == 2]
-
-actual_not = []
-for i in copy_actual_not['Conversation']:
-    review = i[4:]
-    review = ' '.join(review)
-    actual_not.append(review)
-
-copy_actual_not['Conversation'] = actual_not
-
-actual_words_not = []
-for i in actual_not:
-    review = i.split()
-    for j in review:
-        actual_words_not.append(j)
-
-counts_not = Counter(actual_words_not)
-
-actual_stopwords_not = []
-for i,j in zip(counts_not,counts_not.values()):
-    if(j < 2 or j > 1000):
-        actual_stopwords_not.append(i)
-        
-actual_split_not = []
-for i in copy_actual_not['Conversation']:
-    review = i.split()
-    review = [word for word in review if not word in actual_stopwords_not]
-    actual_split_not.append(' '.join(review))
-
-copy_actual_not['Conversation'] = actual_split_not
-copy_actual_not = copy_actual_not[copy_actual_not['Conversation'] != '']
-
-while '' in actual_split_not:
-    actual_split_not.remove('')
-
-X_1 = cv.fit_transform(actual_split_not).toarray()
-V_1 = cv.vocabulary_
-B_1 = cv.get_feature_names()
+X_1,_,_,copy_actual_not = preprocess('Cluster Level 2',2,copy)
 
 dec_not = DEC(dims=[X_1.shape[-1], 500, 500, 2000, 10], n_clusters=3, init=init)
 
@@ -212,20 +189,5 @@ y_pred_not = dec_not.fit(X_1, tol=tol, maxiter=maxiter, batch_size=batch_size,
                  update_interval=update_interval, save_dir=save_dir)
 
 copy_actual_not['Cluster Level 3'] = list(y_pred_not)
-
-for j in range(3):
-    n = []
-    for i in copy_actual_not['Conversation'][copy_actual_not['Cluster Level 3'] == j]:
-        n.append(i)
-    
-    wordcloud = WordCloud(width = 800, height = 800, 
-                    background_color ='white', 
-                    stopwords = stopwords, 
-                    min_font_size = 10).generate(' '.join(n)) 
-    
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud) 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
-    plt.savefig('Not Satisfied WordCloud for Cluster '+str(j), dpi = 500)  
-    plt.show()
+        
+make_wordcloud(3,copy_actual_not,'Cluster Level 3','Not Satisfied DEC Cluster')
